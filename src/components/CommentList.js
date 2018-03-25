940e454965ab1fef21b2a3b107b0e48924a78493
BigFriend <Penny@Penny.local>
2018-03-25 18:06:44 +0800

    css

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -20,8 +20,54 @@ class CommentList extends Component {
               to='/commentDetail'
             >
             <li key={comment.id} className='comment-grid'>
-              {comment.title
-                + '\n' + comment.body}
+              <div className="cotent-row">
+                <div className="text-left">
+                    Title:
+                </div>
+                <div className="text-right">
+                    {comment.title}
+                </div>
+              </div>
+              <div className="cotent-row">
+                <div className="text-left">
+                    Body:
+                </div>
+                <div className="text-right">
+                    {comment.body}
+                </div>
+              </div>
+              <div className="cotent-row">
+                <div className="text-left">
+                    Author:
+                </div>
+                <div className="text-right">
+                    {comment.author}
+                </div>
+              </div>
+              <div className="cotent-row">
+                <div className="text-left">
+                    CommentCount:
+                </div>
+                <div className="text-right">
+                    {comment.commentCount}
+                </div>
+              </div>
+              <div className="cotent-row">
+                <div className="text-left">
+                    VoteScore:
+                </div>
+                <div className="text-right">
+                    {comment.voteScore}
+                </div>
+              </div>
+              <div className="cotent-row">
+                <div className="text-left">
+                    Time:
+                </div>
+                <div className="text-right">
+                    {comment.timestamp}
+                </div>
+              </div>
             </li>
             </Link>
           ))}
